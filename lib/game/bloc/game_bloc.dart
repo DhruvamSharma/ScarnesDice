a39b4d1df4fbe320af6454cdc55b8b1bbeaa14cf
DhruvamSharma <dhruvamssharma@gmail.com>
2025-03-01 17:31:10 +0530

    added code for milestone 5

--- lib/game/bloc/game_bloc.dart
+++ lib/game/bloc/game_bloc.dart
@@ -4,9 +4,9 @@ import 'dart:math';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-part 'game_state.dart';
-
 part 'game_events.dart';
+
+part 'game_state.dart';
 
 const _computerHoldThreshold = 20;
 
@@ -36,7 +36,10 @@ class GameBloc extends Bloc<GameEvent, GameState> {
     }
   }
 
-  void _onHold(Hold event, Emitter<GameState> emit) {
+  FutureOr<void> _onHold(Hold event, Emitter<GameState> emit) {
+    // add the player's turn score to the player's total score
+    // reset the player's turn score
+    // emit the new state
     if (state.isPlayerTurn) {
       emit(
         state.copyWith(
@@ -49,7 +52,7 @@ class GameBloc extends Bloc<GameEvent, GameState> {
       emit(
         state.copyWith(
           computerTotalScore:
-              state.computerTotalScore + state.computerTurnScore,
+          state.computerTotalScore + state.computerTurnScore,
           computerTurnScore: 0,
           isPlayerTurn: true,
         ),
@@ -57,15 +60,16 @@ class GameBloc extends Bloc<GameEvent, GameState> {
     }
   }
 
-  void _onReset(Reset event, Emitter<GameState> emit) {
+  FutureOr<void> _onReset(Reset event, Emitter<GameState> emit) {
     // reset the player's total score and turn score
     // reset the computer's total score and turn score
     // emit the new state
     emit(const GameState.initial());
   }
 
-  FutureOr<void> _onComputerTurn(ComputerTurn event, Emitter<GameState> emit) {
-    // disable the hold and roll buttons
+  Future<void> _onComputerTurn(
+      ComputerTurn event, Emitter<GameState> emit) async {
+    // disable the player's turn
     emit(
       state.copyWith(
         isPlayerTurn: false,
@@ -74,21 +78,26 @@ class GameBloc extends Bloc<GameEvent, GameState> {
 
     // computer will keep rolling until it reaches the hold threshold
     // or rolls a 1
-    while (state.computerTurnScore < _computerHoldThreshold) {
-      final diceValue = 1 + Random().nextInt(6);
-      emit(
-        state.copyWith(
-          currentDiceValue: diceValue,
-          computerTurnScore:
-              diceValue == 1 ? 0 : state.computerTurnScore + diceValue,
-        ),
-      );
+    final diceValue = 1 + Random().nextInt(6);
+    emit(
+      state.copyWith(
+        currentDiceValue: diceValue,
+        computerTurnScore:
+        diceValue == 1 ? 0 : state.computerTurnScore + diceValue,
+      ),
+    );
 
-      if (diceValue == 1) {
-        break;
-      }
+    if (diceValue == 1 || state.computerTurnScore >= _computerHoldThreshold) {
+      add(Hold());
+    } else {
+      _delayedCall(() {
+        add(ComputerTurn());
+      });
     }
+  }
 
-    add(Hold());
+  void _delayedCall(Function action) {
+    Future<dynamic>.delayed(const Duration(seconds: 1))
+        .whenComplete(() => action.call());
   }
 }
