d116e281b1380642e7cd3ee017c7d7df05be4d85
DhruvamSharma <dhruvamssharma@gmail.com>
2025-03-01 17:09:43 +0530

    added code for milestone 3

--- lib/game/bloc/game_bloc.dart
+++ lib/game/bloc/game_bloc.dart
@@ -14,15 +14,31 @@ class GameBloc extends Bloc<GameEvent, GameState> {
   }
 
   void _onRollDice(Roll event, Emitter<GameState> emit) {
+    // select a random number between 1 and 6
     final diceValue = 1 + Random().nextInt(6);
+    // if the dice value is 1, reset the turn score
+    // otherwise, add the dice value to turn score
+    emit(
+        state.copyWith(
+          currentDiceValue: diceValue,
+          playerTurnScore: diceValue == 1 ? 0 : state.playerTurnScore + diceValue,
+        ),
+    );
+  }
+
+  void _onHold(Hold event, Emitter<GameState> emit) {
     emit(
       state.copyWith(
-        currentDiceValue: diceValue,
+        playerTotalScore: state.playerTotalScore + state.playerTurnScore,
+        playerTurnScore: 0,
       ),
     );
   }
 
-  void _onHold(Hold event, Emitter<GameState> emit) {}
-
-  void _onReset(Reset event, Emitter<GameState> emit) {}
+  void _onReset(Reset event, Emitter<GameState> emit) {
+    // reset the player's total score and turn score
+    // reset the computer's total score and turn score
+    // emit the new state
+    emit(const GameState.initial());
+  }
 }
